a049e31799fb6956672727cbe193079bee5c0253
sacultang <sacultang@gmail.com>
2022-05-07 18:29:02 +0900

    feat : goal btn area drag add check-boundary add

--- js/index.js
+++ js/index.js
@@ -3,12 +3,40 @@ const historyWrap = document.querySelector('#history-wrap')
 const dragArea = document.querySelector('.drag-area')
 const header = document.querySelector('#header')
 const adText = document.querySelector('.ad-text')
-
 let isDown = false
 let initY
 let firstY
-// console.log(historyWrap.getBoundingClientRect())
-// console.log(section.getBoundingClientRect())
+// goalBtn
+const goalWrap = document.querySelector('.goal-wrap')
+const goalSlider = document.querySelector('.goal-slider')
+let isLeft = false
+let initX
+let firstX
+
+// goal Event
+goalWrap.addEventListener('mousedown', (e) => {
+  e.preventDefault
+  isLeft = true
+  initX = goalSlider.offsetLeft
+  firstX = e.pageX
+
+  goalWrap.style.cursor = 'grabbing'
+  goalWrap.addEventListener('mousemove', dragLeft, false)
+  window.addEventListener(
+    'mouseup',
+    () => {
+      goalWrap.removeEventListener('mousemove', dragLeft, false)
+    },
+    false
+  )
+})
+const dragLeft = (e) => {
+  // console.log(e)
+  goalSlider.style.left = initX + e.pageX - firstX + 'px'
+  checkGoalBoundary()
+}
+
+// historyWrap event
 dragArea.addEventListener('mousedown', (e) => {
   e.preventDefault
   isDown = true
@@ -26,32 +54,31 @@ dragArea.addEventListener('mousedown', (e) => {
   )
 })
 
-function dragIt(e) {
+const dragIt = (e) => {
   historyWrap.style.top = initY + e.pageY - firstY + 'px'
   checkboundary()
 }
-function checkboundary() {
+const checkboundary = () => {
   let headerOuter = header.getBoundingClientRect()
   let historyInner = historyWrap.getBoundingClientRect()
   let adOuter = adText.getBoundingClientRect()
-
   if (historyInner.top <= headerOuter.bottom) {
     historyWrap.style.top = `${headerOuter.top}px`
   } else if (historyInner.top >= adOuter.bottom) {
     historyWrap.style.top = `${adOuter.bottom - 50}px`
   }
-  // console.log(adOuter)
-  // console.log('history-bottom', historyInner.bottom)
-  // console.log('historyinner-top', historyWrap.style.top)
 }
-
-// const goalWrap = document.querySelectorAll('.goal-wrap')
-// console.log(goalWrap[0])
-// goalWrap.forEach((item) => {
-//   item.addEventListener('touchstart', (e) => {
-//     console.log(e.changedTouches[0])
-//   })
-// })
+const checkGoalBoundary = () => {
+  let goalWrapOuter = goalWrap.getBoundingClientRect()
+  let goalSliderInner = goalSlider.getBoundingClientRect()
+  if (goalSliderInner.right <= goalWrapOuter.right) {
+    goalSlider.style.left = `${
+      goalWrapOuter.width - goalSliderInner.width - 10
+    }px`
+  } else if (parseInt(goalSlider.style.left) >= 0) {
+    goalSlider.style.left = `0px`
+  }
+}
 
 // PopUp
 const graphBtn = document.querySelector('.graph-btn')
