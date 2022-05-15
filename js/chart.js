7f794badcfdaa5b65ec640ba955de6238c3015b9
sacultang <sacultang@gmail.com>
2022-05-16 04:39:03 +0900

    ??

--- js/chart.js
+++ js/chart.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 // Featch Data
 const url =
   'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4b41cea6-0a74-4f33-86b8-718f24260cc3/data.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220515%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220515T184308Z&X-Amz-Expires=86400&X-Amz-Signature=863b77cd5c7a0c2d121d49479f1a129ac2677cabfbc56c088a1b2bfb0282291c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22data.json%22&x-id=GetObject';
@@ -9,7 +9,7 @@ const getData = async () => {
   dailyArrData(data);
   monthArrData(data);
 };
-getData();
+
 // GroupBy 모듈
 const groupBy = (data, key) => {
   return data.reduce((carry, el) => {
@@ -49,11 +49,11 @@ let dailyValues = [];
 
 const dailyArrData = (data) => {
   // 일별로 데이터 분류
-  // const dateArr = groupBy(data, 'date');
-  const lodashDateArr = _.groupBy(data, 'date');
+  const dateArr = groupBy(data, 'date');
+  // const dateArr = _.groupBy(data, 'date');
 
   // 날짜를 기준으로 나머지 데이터 묶음
-  for (const [dateText, dailyList] of Object.entries(lodashDateArr)) {
+  for (const [dateText, dailyList] of Object.entries(dateArr)) {
     const totalPrice = amount(dailyList);
     dailyLabels.push(dateText); // 날짜만 push
     dailyValues.push(totalPrice); // 총 합 push
@@ -112,11 +112,11 @@ const monthArrData = (data) => {
   });
 
   // 4월 데이터를 타입으로 분류
-  // const aprilTypeArr = groupBy(aprilData, 'type');
-  const lodashAprilTypeArr = _.groupBy(aprilData, 'type');
+  const aprilTypeArr = groupBy(aprilData, 'type');
+  // const aprilTypeArr = _.groupBy(aprilData, 'type');
 
   // 타입을 기준으로 나머지 묶음
-  for (const [typeText, typeValue] of Object.entries(lodashAprilTypeArr)) {
+  for (const [typeText, typeValue] of Object.entries(aprilTypeArr)) {
     // 타입 총합 구하기
     const totalPrice = amount(typeValue);
 
@@ -234,3 +234,4 @@ const myChart = new Chart(monthCtx, {
     },
   },
 });
+getData();
